21d0a6df979afd063d07c4c19d101fc0d6cf232b
Shingai <shingai@ip-172-31-41-42.ec2.internal>
2020-04-07 23:29:24 +0000

    making fixes to getting_started docs

--- scripts/loadModelingEnv.R
+++ scripts/loadModelingEnv.R
@@ -10,7 +10,7 @@ library(devtools)
 
 
 #install CRAN packages
-lib<-c("devtools","SDMtune","INWT/INWTUtils","Quandl","magrittr","caret","gbm","glmnet","randomForest","kernlab","doParallel","tidyverse","knitr","asobalife/lookr","rJava","dplyr","snowflakedb/dplyr-snowflakedb","zeallot","glue","httr","jsonlite","bigrquery","rich1000/lookr")
+lib<-c("devtools","tidyr","SDMtune","INWT/INWTUtils","Quandl","magrittr","ggplot2","caret","gbm","glmnet","randomForest","kernlab","doParallel","tidyverse","knitr","asobalife/lookr","rJava","dplyr","dbplyr","zeallot","glue","httr","jsonlite","bigrquery","rich1000/lookr")
 packages(lib, prompt = FALSE)
 libraries(lib)
 
