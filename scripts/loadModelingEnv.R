4e7b875b7532b261a5cfe89f0ff6fad81fb625e2
Shingi <samudzi@gmail.com>
2020-04-07 10:30:11 -0700

    adding Quandl to pre-installed packages

--- scripts/loadModelingEnv.R
+++ scripts/loadModelingEnv.R
@@ -10,7 +10,7 @@ library(devtools)
 
 
 #install CRAN packages
-lib<-c("devtools","SDMtune","INWT/INWTUtils","magrittr","caret","gbm","glmnet","randomForest","kernlab","doParallel","tidyverse","knitr","asobalife/lookr","rJava","dplyr","snowflakedb/dplyr-snowflakedb","zeallot","glue","httr","jsonlite","bigrquery","rich1000/lookr")
+lib<-c("devtools","SDMtune","INWT/INWTUtils","Quandl","magrittr","caret","gbm","glmnet","randomForest","kernlab","doParallel","tidyverse","knitr","asobalife/lookr","rJava","dplyr","snowflakedb/dplyr-snowflakedb","zeallot","glue","httr","jsonlite","bigrquery","rich1000/lookr")
 packages(lib, prompt = FALSE)
 libraries(lib)
 
